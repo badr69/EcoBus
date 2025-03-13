0edccaaef6e2a6e573212f8988a5e09e718d01f5
badreddine <manoudb@yahoo.fr>
2025-03-13 10:42:13 +0100

    add html

--- app.py
+++ app.py
@@ -1,64 +1,109 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from config import Config
+import os
 from db import get_db_connection
-from forms import *
-
+from forms import RegisterForm, LoginForm, ContactForm, TripForm
+import time
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
-
-
-# TODO: afficher tous les trajets possibles
+#TODO: afficher tous les trajets
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-    # conn = get_db_connection()
-    # cur = conn.cursor()
-    # cur.execute('SELECT * FROM trips')
-    # trips = cur.fetchall()
-    # cur.close()
-    # conn.close()
-    # return render_template('index.html', trips=trips)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM trips')
+    trips = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', trips=trips)
 
 
-# TODO: create a new trip
-@app.route('/create_trip', methods=['GET', 'POST'])
-def create_trip():
+# TODO: ajouter un nouveau trajet
+@app.route('/add_trip', methods=['GET', 'POST'])
+def add_trip():
+    tripform = TripForm()
     if request.method == 'POST':
-        depart = request.form['depart']
+        departure = request.form['departure']
         destination = request.form['destination']
+        departure_time = request.form['departure_time']
+        available_seats = request.form['available_seats']
         conn = get_db_connection()
         cur = conn.cursor()
-        cur.execute('INSERT INTO trips (depart, destination) VALUES (%s, %s))', (depart, destination))
+        cur.execute('INSERT INTO trips (departure, destination, departure_time_date, available_seats) VALUES (%s, %s, %s, %s)', (departure, destination, departure_time, available_seats))
         conn.commit()
         conn.close()
         cur.close()
         return redirect(url_for('index'))
-    render_template('create_trip.html')
+    return render_template('add_trip.html', form=tripform)
 
-
+# TODO: edit a specific trip
+# TODO: update a specific trip
+#
+# TODO: route pour la page  de contact
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     contactform = ContactForm()
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        content = request.form['content']
     return render_template('contact.html', form=contactform)
 
+
 # TODO: routes for auth
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     registerform = RegisterForm()
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        role = request.form['role']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO passengers (name, email, role, password) VALUES (%s, %s, %s, %s)',
+                    (name, email, role, password))
+        conn.commit()
+        conn.close()
+        cur.close()
+        flash('Inscription réussie!', 'success')
+        return redirect(url_for('login'))
     return render_template("auth/register.html", form=registerform)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     loginform = LoginForm()
-    return render_template("auth/login.html", form=LoginForm())
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM passengers WHERE email = %s AND password = %s', (email, password))
+        passenger = cur.fetchone()
+        conn.close()
+        cur.close()
+
+        if passenger:
+            session['passenger_id'] = passenger[0]
+            return redirect(url_for('index'))
+        else:
+            return "nom d'utilisateur incorrect", 401
+    return render_template("auth/login.html", form=loginform)
+
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for('index'))
 
 
 
-# TODO: edit a specific trip
-# TODO: update a specific trip
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,7 +1,20 @@
 from wtforms import validators
-from wtforms.fields.simple import StringField, TextAreaField, SearchField, SubmitField, BooleanField, PasswordField
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, IntegerField
 from wtforms.form import Form
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
+from psycopg2 import Timestamp
+from wtforms import DateField
+
+
+class TripForm(Form):
+    departure = StringField('Departure', validators=[DataRequired(), Length(min=1, max=50)])
+    destination = StringField('Destination', validators=[DataRequired(), Length(min=1, max=50)])
+    departure_time = DateField('Departure Time', format='%y-%m-%d %H:%M', validators=[DataRequired(), Length(min=5, max=50)])
+
+    available_seats = IntegerField('Available Seats', validators=[DataRequired(), Length(min=1, max=50)])
+    created_at = Timestamp
+
+
 
 
 class ContactForm(Form):
@@ -10,13 +23,19 @@ class ContactForm(Form):
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+# TODO: auth forms
 class RegisterForm(Form):
     name = StringField('name', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    role = StringField('Role', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
     submit = SubmitField('Submit')
-    accept_rules = BooleanField('I accept the site rules', [validators.InputRequired()])
 
 class LoginForm(Form):
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     password = PasswordField('Password', [validators.DataRequired()])
     submit = SubmitField('Submit')
+
+class BookingForm(Form):
+    trip_id = IntegerField('Trip ID', validators=[DataRequired()])
+    passenger_id = IntegerField('Passenger ID', validators=[DataRequired()])
